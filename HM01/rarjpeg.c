6063329ae724b3b4c361b04bd678da46d4438aff
NikWine <156602902+NikWine@users.noreply.github.com>
2024-01-21 18:50:39 +0300

    Add files via upload

--- HM01/rarjpeg.c
+++ HM01/rarjpeg.c
@@ -1,48 +1,67 @@
-//Compile with gcc -o ftest ftest.c -Wall -Wextra -Wpedantic -std=c11
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-//#define RAR_SIGNATURE "Rar"
 #define ZIP_MAGIC_HEADER "\x50\x4B\x03\x04"
 
-//Функция проверки файла на rarjpeg
+// Функция для проверки, является ли файл Rarjpeg
 int is_rarjpeg(const char *filename) {
-// Открытие файла на чтение в бинарном режиме
+    // Открытие файла для чтения в бинарном режиме
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         perror("fopen");
         return 0;
     }
 
-  printf("In rarjpeg\n");
+    printf("In rarjpeg\n");
 
-  char header[4];
-  int position, i, sz;
-  //Опеределение размера файла
-  fseek(file, 0L, SEEK_END);
-  sz = ftell(file);
-  //Поиск заголовка zip со смещением по 4 байта
-  fseek(file, 0, SEEK_SET);
-  for (i = 1; i < sz; i++) {
-    fseek(file, i, SEEK_SET);
-    fread(header, 4, 1, file);
-    if (memcmp(header, ZIP_MAGIC_HEADER, 4) == 0) {
-        position=i;
-        printf("position %i", position);
-        return 1;
+    char header[4];
+    int position, i, sz;
+    // Определение размера файла
+    fseek(file, 0L, SEEK_END);
+    sz = ftell(file);
+    // Поиск заголовка ZIP со смещением в 4 байта
+    fseek(file, 0, SEEK_SET);
+    for (i = 1; i < sz; i++) {
+        fseek(file, i, SEEK_SET);
+        fread(header, 4, 1, file);
+        if (memcmp(header, ZIP_MAGIC_HEADER, 4) == 0) {
+            position=i;
+            printf("position %i", position);
+            return 1;
+        }
     }
-  }
 
     return 0;
 }
-// Функция для извлечения списка файлов из RAR архива
+
+// Функция для получения списка файлов из RAR архива
 void list_rar_files(const char *filename) {
-    char command[256];
-    //Сборка команды для системного вызова unzip с параметрами -l  list files (short format) -v  list verbosely/show version info
-    snprintf(command, sizeof(command), "unzip -lv \"%s\"", filename);
-    //Запуск 
-    system(command);
+    // Создание процесса
+    pid_t pid = fork();
+    
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Испытательная работа извлекается в дочернем процессе
+    if (pid == 0) {
+        execlp("unzip", "unzip", "-lv", filename, NULL);
+        perror("exec");
+        exit(EXIT_FAILURE);
+    } 
+    // Родительский процесс считывает статус завершения дочернего процесса
+    else {
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            fprintf(stderr, "Process exited with error %d\n", WEXITSTATUS(status));
+            exit(EXIT_FAILURE);
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
