a7ba0af8159e1146a999a10a973280942109f0ee
NikWine <156602902+NikWine@users.noreply.github.com>
2024-01-28 04:40:47 +0300

    Add files via upload

--- HM14/Asynchronous HTTP Server.c
+++ HM14/Asynchronous HTTP Server.c
@@ -9,203 +9,162 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
-#define MAX_EVENTS 64
-#define BUFFER_SIZE 1024
+#define MAXEVENTS 64
+#define BUFFSIZE 1024
 
-// Объявление функций
-int set_nonblocking(int sockfd);
-void send_http_response(int sockfd, const char *status, const char *content_type, const char *body, int body_length);
-void send_file_contents(int client_fd, int file_fd);
-void handle_http_request(int sockfd, const char *dir);
-
-// Функция для установки сокета в неблокирующий режим
-int set_nonblocking(int sockfd) {
-    int flags;
-    if ((flags = fcntl(sockfd, F_GETFL, 0)) == -1) {
-        flags = 0;
+// Сделать сокет неблокирующим
+int make_socket_non_blocking(int sock) {
+    int flags = fcntl(sock, F_GETFL, 0);
+    if (flags == -1) {
+        return -1;
     }
-    return fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+    flags |= O_NONBLOCK;
+    return fcntl(sock, F_SETFL, flags);
 }
 
-// Функция для отправки содержимого файла
-void send_file_contents(int client_fd, int file_fd) {
-    char buffer[BUFFER_SIZE];
+// Доставить данные файла
+void deliver_file_data(int client, int file) {
+    char buffer[BUFFSIZE];
     ssize_t bytes_read;
-
-    while ((bytes_read = read(file_fd, buffer, BUFFER_SIZE)) > 0) {
-        if (send(client_fd, buffer, bytes_read, 0) == -1) {
-            perror("send");
+    while ((bytes_read = read(file, buffer, sizeof(buffer))) > 0) {
+        if (send(client, buffer, bytes_read, 0) == -1) {
+            perror("Не удалось отправить данные файла");
             break;
         }
-    }   
+    }
 }
 
-// Функция для отправки HTTP ответа
-void send_http_response(int sockfd, const char *status, const char *content_type, const char *body, int body_length) {
-    char header[BUFFER_SIZE];
+// Отправить HTTP-ответ
+void send_http_response(int fd, const char *status_code, const char *content_type, const char *body, int body_length) {
+    char response_header[BUFFSIZE];
+    const char *body_content = body ? body : "";
+    const char *mime_type = content_type ? content_type : "text/html; charset=utf-8";
+    
+    int content_length = body_length >= 0 ? body_length : (int)strlen(body_content);
 
-    if (body == NULL) body = "";
-    if (content_type == NULL) content_type = "text/html; charset=utf-8";
-    if (body_length < 0) body_length = strlen(body);
-
-    snprintf(header, sizeof(header),
-        "HTTP/1.1 %s\r\n"
-        "Content-Type: %s\r\n"
-        "Content-Length: %d\r\n"
-        "\r\n",
-        status, content_type, body_length);
-    if (send(sockfd, header, strlen(header), 0) == -1) {
-        perror("send");
-        return;
-    }   
-
-    if (body_length > 0 && send(sockfd, body, body_length, 0) == -1) {
-    perror("send");
-    }   
+    snprintf(response_header, sizeof(response_header), "HTTP/1.1 %s\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n",
+             status_code, mime_type, content_length);
+    send(fd, response_header, strlen(response_header), 0);
+    if (content_length > 0) {
+        send(fd, body_content, content_length, 0);
+    }
 }
 
 // Функция для обработки HTTP запроса
-void handle_http_request(int sockfd, const char *dir) {
-    char buffer[BUFFER_SIZE];
-    ssize_t nbytes = recv(sockfd, buffer, sizeof(buffer), 0);
+void handle_http_request(int client_fd, const char *web_root) {
+    char req_buffer[BUFFSIZE];
+    ssize_t bytes_received = recv(client_fd, req_buffer, sizeof(req_buffer) - 1, 0);
+    
 
-    // Проверяем получено ли что-то от sockfd
-    if (nbytes <= 0) {
-        if (nbytes < 0) {
-            perror("recv");
-        }
-    } else {
-        buffer[nbytes] = '\0';
-
-        // Если начальные символы буфера - это 'GET '
-        if (strncmp(buffer, "GET ", 4) == 0) {
-            char *path = buffer + 4;
-            char *end_path = strchr(path, ' ');
-        
-            if (end_path) {
-                *end_path = '\0';
-                char full_path[BUFFER_SIZE];
-                snprintf(full_path, sizeof(full_path), "%s%s", dir, path);
-
-                // Пытаемся открыть файл
-                int file_fd = open(full_path, O_RDONLY);
-                if (file_fd == -1) {
-                    if (errno == EACCES) {
-                        send_http_response(sockfd, "403 Forbidden", "text/plain", "403 Forbidden: Доступ запрещен.", -1);
-                    } else {
-                        send_http_response(sockfd, "404 Not Found", "text/plain", "404 Not Found: Запрошенный ресурс не найден на этом сервере.", -1);
-                    }
+    if (bytes_received > 0) {
+        req_buffer[bytes_received] = '\0';
+        if (strncmp(req_buffer, "GET ", 4) == 0) {
+            char *path_start = req_buffer + 4;
+            char *path_end = strchr(path_start, ' ');
+            if (path_end) {
+                *path_end = '\0';
+                char full_file_path[BUFFSIZE];
+                snprintf(full_file_path, sizeof(full_file_path), "%s%s", web_root, path_start);
+                int file_desc = open(full_file_path, O_RDONLY);
+                if (file_desc != -1) {
+                    send_http_response(client_fd, "200 OK", "text/plain", NULL, -1);
+                    deliver_file_data(client_fd, file_desc);
+                    close(file_desc);
                 } else {
-                    send_http_response(sockfd, "200 OK", "text/plain", NULL, 0);
-                    send_file_contents(sockfd, file_fd);
-                    close(file_fd);
+                    // Отправляем ответ 403 Forbidden в случае ошибки доступа или 404 Not Found для остальных ошибок
+                    if (errno == EACCES) {
+                        send_http_response(client_fd, "403 Forbidden", "text/plain", "403 Forbidden: Доступ запрещен.", -1);
+                    } else {
+                        send_http_response(client_fd, "404 Not Found", "text/plain", NULL, -1);
+                    }
                 }
             }
-        } else {
-            send_http_response(sockfd, "400 Bad Request", "text/plain", "400 Bad Request: Запрос не может быть выполнен из-за неправильного синтаксиса.", -1);
         }
     }
-    close(sockfd);
+    close(client_fd);
 }
 
 int main(int argc, char *argv[]) {
-
-    // Если не заданы необходимые аргументы, выводим сообщение и завершаем выполнение
-    if (argc < 4) {
-        fprintf(stderr, "Usage: %s <каталог> <адресс> <порт>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s [port] [web root]\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    const char *dir = argv[1];
-    const char *address = argv[2];
-    int port = atoi(argv[3]);
+    int server_fd, new_client, epoll_fd;
+    struct sockaddr_in server_addr;
+    struct epoll_event event, events[MAXEVENTS];
 
-    // Создаем сокет
-    int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_sock == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-    set_nonblocking(listen_sock);
-
-    // Задаем адрес и порт для сокета
-    struct sockaddr_in srv_addr;
-    memset(&srv_addr, 0, sizeof(srv_addr));
-    srv_addr.sin_family = AF_INET;
-    srv_addr.sin_addr.s_addr = inet_addr(address);
-    srv_addr.sin_port = htons(port);
-
-    // Привязываем сокет к определенному адресу и порту
-    if (bind(listen_sock, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) == -1) {
-        perror("bind");
-        exit(EXIT_FAILURE);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Не удалось создать сокет");
+        return EXIT_FAILURE;
     }
 
-    //Если ошибка слушания, выводим сообщение об ошибке и выходим
-    if (listen(listen_sock, SOMAXCONN) == -1) {
-        perror("listen");
-        exit(EXIT_FAILURE);
+    int port = atoi(argv[1]);
+    const char *web_root = argv[2];
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Could not bind socket");
+        return EXIT_FAILURE;
     }
 
-    // Создаем epoll файловый дескриптор
-    int epoll_fd = epoll_create1(0);
+    if (listen(server_fd, SOMAXCONN) == -1) {
+        perror("Could not listen on socket");
+        return EXIT_FAILURE;
+    }
+
+    epoll_fd = epoll_create1(0);
     if (epoll_fd == -1) {
-        perror("epoll_create1");
-        exit(EXIT_FAILURE);
+        perror("Could not create epoll instance");
+        return EXIT_FAILURE;
     }
 
-    struct epoll_event event;
-    event.data.fd = listen_sock;
+    event.data.fd = server_fd;
     event.events = EPOLLIN | EPOLLET;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_sock, &event) == -1) {
-        perror("epoll_ctl");
-        exit(EXIT_FAILURE);
-    }
-
-    struct epoll_event *events = calloc(MAX_EVENTS, sizeof(event));
-    if (!events) {
-        perror("calloc");
-        exit(EXIT_FAILURE);
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+        perror("Не удалось добавить серверный сокет в epoll");
+        return EXIT_FAILURE;
     }
 
     // Основной цикл обработки событий
     while (1) {
-        int n = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        int n = epoll_wait(epoll_fd, events, MAXEVENTS, -1);
         for (int i = 0; i < n; i++) {
-            if (events[i].data.fd == listen_sock) {
-                while (1) {
-                    struct sockaddr in_addr;
-                    socklen_t in_len = sizeof(in_addr);
-                    int conn_sock = accept(listen_sock, &in_addr, &in_len);
-                    if (conn_sock == -1) {
-                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                            break; // Обработали все соединения
-                        } else {
-                            perror("accept");
-                            break;
-                        }
+            if (events[i].data.fd == server_fd) {
+                struct sockaddr in_addr;
+                socklen_t in_len = sizeof(in_addr);
+                new_client = accept(server_fd, &in_addr, &in_len);
+                if (new_client == -1) {
+                    if ((errno != EAGAIN) && (errno != EWOULDBLOCK)) {
+                        perror("Could not accept client connection");
                     }
+                    continue;
+                }
 
-                    //Устанавливаем неблокирующий флаг для сокета
-                    set_nonblocking(conn_sock);
-                    event.data.fd = conn_sock;
-                    event.events = EPOLLIN | EPOLLET;
-                    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conn_sock, &event) == -1) {
-                        perror("epoll_ctl");
-                        close(conn_sock);
-                    }
-                }    
+                if (make_socket_non_blocking(new_client) == -1) {
+                    perror("Could not make client socket non-blocking");
+                    close(new_client);
+                    continue;
+                }
+
+                event.data.fd = new_client;
+                event.events = EPOLLIN | EPOLLET;
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_client, &event) == -1) {
+                    perror("Не удалось добавить клиентский сокет в epoll");
+                    close(new_client);
+                    continue;
+                }
             } else {
-
-                // Обработка HTTP-запроса 
-                handle_http_request(events[i].data.fd, dir);
+                handle_http_request(events[i].data.fd, web_root);
             }
         }
     }
 
-    close(epoll_fd);
-    free(events);
-    close(listen_sock);
-
+    close(server_fd);
     return EXIT_SUCCESS;
 }
